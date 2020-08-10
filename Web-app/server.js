b45e1274b7e074dc51776aafc4adc799200c6be1
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-08-10 07:52:59 -0700

    Update server.js

--- Web-app/server.js
+++ Web-app/server.js
@@ -7,11 +7,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('/tmp/node-app-inst/Web-app/views/index');
 })
 app.post('/', function (req, res) {
   console.log(req.body.city);
-  res.render('Web-app/views/index');  
+  res.render('/tmp/node-app-inst/Web-app/index');  
 })
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
