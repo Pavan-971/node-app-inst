cbc42d07a42e897a73185de2d3df3bf498833629
Pavan-971 <69103190+Pavan-971@users.noreply.github.com>
2020-08-10 08:17:01 -0700

    Update server.js

--- Web-app/server.js
+++ Web-app/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express()
 
 
-
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   res.render('/tmp/node-app-inst/Web-app/views/index');
